6b4f50e95a0eabb827a6dd1dbdb5b15dcfa68029
MoonRiverRan <280720627@qq.com>
2021-07-27 20:04:01 +0800

    1.ROC曲线以及对应的AUC 2.PR曲线以及对应的average_precision 3.epochs和auc的关系曲线

--- code/main.py
+++ code/main.py
@@ -4,20 +4,19 @@ from param import parameter_parser
 from MMGCN import MMGCN
 from dataprocessing import data_pro
 import torch
-import pandas
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+import matplotlib.pyplot as plt
 
-def train(model, train_data, optimizer, opt):
+def train(model, train_data, optimizer, epochs):
     model.train()
-    for epoch in range(0, opt.epoch):
+    for epoch in range(0, epochs):
         model.zero_grad() ## 梯度清零
         score = model(train_data) ## 得到预测值
         loss = torch.nn.MSELoss(reduction='mean')  ## 求解loss
         loss = loss(score, train_data['md_p'].to(device))
         loss.backward() ## 反向传播求解梯度
         optimizer.step() ## 更新权重参数
-        print("loss: ",loss.item())
+        #print("loss: ",loss.item())
     score = score.detach().cpu().numpy()
     scoremin, scoremax = score.min(), score.max()
     score = (score - scoremin) / (scoremax - scoremin) #Min-Max归一化方法
@@ -25,14 +24,43 @@ def train(model, train_data, optimizer, opt):
 
 
 def main():
+    from sklearn.metrics import roc_curve, auc
     args = parameter_parser()
     dataset = data_pro(args)
     train_data = dataset
     model = MMGCN(args)
     model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    score = train(model, train_data, optimizer, args)
-    df = pd.DataFrame(score)
-    df.to_excel(r"C:\Users\23922\Desktop\test.xlsx", sheet_name='Sheet1', index=False)
+    aucs = []
+    aucs.append(0)
+    epochs = []
+    epochs.append(0)
+    df2 = pd.read_csv("../datasets/m_d.csv")
+    y = df2
+    y = y.to_numpy().flatten()
+    for i in range (1,700):
+        scores = train(model, train_data, optimizer, i)
+        df1 = pd.DataFrame(scores)
+        y_pred = df1.iloc[0:-1,]
+        y_pred = y_pred.to_numpy().flatten()
+        fpr,tpr,threshold=roc_curve(y,y_pred)
+        AUC = auc(fpr,tpr)
+        aucs.append(AUC)
+        epochs.append(i)
+        print("auc: ",i,"=",AUC)
+
+    print("aucs: ",aucs)
+    plt.figure(figsize=(5,5),dpi=100)
+    plt.plot(epochs,aucs,linestyle='-',label='gcn layer =3')
+
+    plt.xlabel('Epoch')
+    plt.ylabel('GCN layer')
+
+    plt.legend()
+
+    plt.show()
+
+    #df = pd.DataFrame(score)
+    #df.to_excel(r"C:\Users\zhr\Desktop\test.xlsx", sheet_name='Sheet1', index=False)
 if __name__ == "__main__":
     main()
--- code/roc.py
+++ code/roc.py
@@ -1,91 +1,45 @@
 import numpy as np
 import pandas as pd
-from matplotlib import pyplot as plt
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LogisticRegression
-
-train = pd.read_csv("../datasets/train.csv")
-# 通过info查看数据信息
-train.info()
-
-# 对分类变量进行填充
-train['Cabin'] = train['Cabin'].fillna('NA')
-train['Embarked'] = train['Embarked'].fillna('S')
-
-# 对连续变量进行填充
-train['Age'] = train['Age'].fillna(train['Age'].mean())
-
-train.isnull().mean().sort_values(ascending=False)
-data = train[['PassengerId','Pclass','Sex','Age','SibSp','Parch','Fare', 'Embarked']]
-data = pd.get_dummies(data)
-data.head()
-
-# 提出Survived作为数据集标签y
-y = train['Survived']
-# 使用sklearn拆分数据集为训练集和测试集
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-# stratify参数是为了保证分布均匀，random_state是为了复现结果，使得每次拆分结果一样
-X_train, X_test, y_train, y_test = train_test_split(data, y, stratify=y, random_state=0)
-# 查看一下拆分后的数据集大小
-print(X_train.shape, X_test.shape)
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_curve,auc
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import average_precision_score
 
 
+df1 = pd.read_excel(r"C:\Users\zhr\Desktop\test.xlsx",index_col=None)
+df2 = pd.read_csv("../datasets/m_d.csv")
+y_pred = df1.iloc[0:-1,]
+y_pred = y_pred.to_numpy().flatten()
+y = df2
+y = y.to_numpy().flatten()
 
-# 默认参数逻辑回归模型
-# lr = LogisticRegression(max_iter=1000)
-# lr.fit(X_train, y_train)
-# # 查看训练集和测试集分数
-# train_score = lr.score(X_train, y_train)
-# test_score = lr.score(X_test, y_test)
-# print(f"train score: {train_score:.2f}")
-# print(f"test score: {test_score:.2f}")
-#
-# # 随机森林分类模型
-# rf = RandomForestClassifier()
-# rf.fit(X_train, y_train)
-# # 查看训练集和测试集分数
-# train_score = rf.score(X_train, y_train)
-# test_score = rf.score(X_test, y_test)
-# print(f"train score: {train_score:.2f}")
-# print(f"test score: {test_score:.2f}")
-#
-from sklearn.model_selection import cross_val_score
-lr = LogisticRegression(max_iter=10000)
-score = []
-alphas = []
-for alpha in range(1,100,1):
-    alphas.append(alpha)
-    sc = np.sqrt(cross_val_score(lr, X_train, y_train, cv=10))
-    score.append(sc.mean())
-plt.plot(alphas,score)
+fpr,tpr,threshold=roc_curve(y,y_pred)
+auc = auc(fpr,tpr)
+
+plt.figure(figsize=(5,5),dpi=100)
+plt.plot(fpr,tpr,linestyle='-',label='MMGCN(auc=%0.3f)'%auc)
+
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+
+plt.legend()
+
 plt.show()
-# # 交叉验证所有分数
-# print(scores)
-# # 交叉验证平均分
-# print(f"Average cross-validation score: {scores.mean():.3f}")
 
 
-#
-# from sklearn.metrics import confusion_matrix
-# # 训练模型
-# lr = LogisticRegression(C=100)
-# lr.fit(X_train, y_train)
-# # 模型预测结果
-# pred = lr.predict(X_train)
-# # 混淆矩阵
-# print(confusion_matrix(y_train, pred))
-#
-# from sklearn.metrics import classification_report
-# # 精确率、召回率以及f1-score
-# print(classification_report(y_train, pred))
-#
-# from sklearn.metrics import roc_curve
-# fpr, tpr, thresholds = roc_curve(y_test, lr.decision_function(X_test))
-# plt.plot(fpr, tpr, label="ROC Curve")
-# plt.xlabel("FPR")
-# plt.ylabel("TPR (recall)")
-# # 找到最接近于0的阈值
-# close_zero = np.argmin(np.abs(thresholds))
-# plt.plot(fpr[close_zero], tpr[close_zero], 'o', markersize=10, label="threshold zero", fillstyle="none", c='k', mew=2)
-# plt.legend(loc=4)
-# plt.show()
+precision, recall, thresholds = precision_recall_curve(y,y_pred)
+plt.plot(recall,precision,linestyle='-',label='MMGCN(auprc=%0.3f)'%auc)
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plt.legend()
+
+average_precision = average_precision_score(y,y_pred)
+
+print('Average precision-recall score: {0:0.2f}'.format(
+      average_precision))
+
+plt.show()
